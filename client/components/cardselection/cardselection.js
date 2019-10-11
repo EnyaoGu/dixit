d52259f9693e1598e6a9370429fedbf3b17bc8e4
Zhenfang Zhu <zhenfang.zhu@ni.com>
2019-10-11 15:05:57 +0800

    Support multiple scenario for card selection (#12)

--- client/components/cardselection/cardselection.js
+++ client/components/cardselection/cardselection.js
@@ -4,7 +4,13 @@ import 'antd/dist/antd.css';
 import PropTypes from 'prop-types';
 import './cardselection.css';
 
-const CARDSELECTION = ({ cards, onCardSelected }) => {
+const PageType = Object.freeze({
+    tellerEnterDescription: 'teller enter description',
+    playerPickCard: 'player Pick his card',
+    vote: 'vote',
+});
+
+const CARDSELECTION = ({ cards, pageType, onCardSelected }) => {
     const [cardDescription, setCardDescription] = useState('');
     const [cardSelected, setCardSelected] = useState('');
 
@@ -27,29 +33,58 @@ const CARDSELECTION = ({ cards, onCardSelected }) => {
                     <Card cover={<img src={cards[3]} />}/>
                 </div>
             </Carousel>
-            <Input
-                id='card-description'
-                placeholder='Enter the description for current card'
-                size='large'
-                onChange={(e) => {
-                    setCardDescription(e.target.value);
-                }}
-                onPressEnter={() => onCardSelected(cardSelected, cardDescription)}
-            />
-            <Button
-                type='primary'
-                onClick={() => onCardSelected(cardSelected, cardDescription)}
-                size='large'
-            >
-                Decide!
-            </Button>
+            <div align={'center'} style={{marginTop:10+'px'}}>
+                { pageType === PageType.tellerEnterDescription ?
+                    <div className={'enter-description-wrapper'}>
+                        <Input
+                            id='card-description'
+                            placeholder='Enter card description'
+                            size='large'
+                            onChange={(e) => {
+                                setCardDescription(e.target.value);
+                            }}
+                            onPressEnter={() => onCardSelected(cardSelected, cardDescription)}
+                        />
+                        <Button
+                            type='primary'
+                            onClick={() => onCardSelected(cardSelected, cardDescription)}
+                            size='large'
+                            style={{marginLeft:5+'px'}}
+                        >
+                            Decide!
+                        </Button>
+                    </div>
+                    : null
+                }
+                { pageType === PageType.playerPickCard ?
+                    <Button
+                        type='primary'
+                        onClick={() => onCardSelected(cardSelected)}
+                        size='large'
+                    >
+                        Use this card
+                    </Button>
+                    : null
+                }
+                { pageType === PageType.vote ?
+                    <Button
+                        type='primary'
+                        onClick={() => onCardSelected(cardSelected)}
+                        size='large'
+                    >
+                        Vote
+                    </Button>
+                    : null
+                }
+            </div>
         </div>
         </>;
 };
 
 CARDSELECTION.propTypes = {
     cards: PropTypes.array.isRequired,
+    pageType: PropTypes.string.isRequired,
     onCardSelected: PropTypes.func.isRequired,
 };
 
-export default CARDSELECTION;
+export { CARDSELECTION, PageType };
