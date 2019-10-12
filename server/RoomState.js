80aa6bf80b0cff46a8005c8612668ff15b10c5a2
Enyao Gu <enyao.gu@ni.com>
2019-10-12 16:17:57 +0800

    Work around room state sync issue

--- server/RoomState.js
+++ server/RoomState.js
@@ -2,10 +2,8 @@ const schema = require('@colyseus/schema');
 const Schema = schema.Schema;
 const ArraySchema = schema.ArraySchema;
 
-class PlayerState extends Schema {
-  constructor (p_player) {
-    super();
-
+class PlayerState {
+  constructor () {
     this.id = '';
     this.name = '';
     this.isTeller = false;
@@ -13,37 +11,24 @@ class PlayerState extends Schema {
     this.holdingCards = new ArraySchema();
     this.voters = new ArraySchema();
     this.usingCard = '';
-    this.voteCard = '';
+    this.votedCard = '';
     this.hasBeenTellerForTimes = 0;
     this.score = 0;
     this.roundScore = 0;
   }
 }
-schema.defineTypes(PlayerState, {
-  id: 'string',
-  name: 'string',
-  isTeller: 'boolean', 
-  holdingCards: [ 'string' ],
-  usingCard: 'string',
-  voteCard: 'string',
-  hasBeenTellerForTimes: 'number',
-  voters: ['string'],
-  score: 'number',
-  roundScore: 'number',
-  isReady: 'boolean',
-});
 
 class RoomState extends Schema {
   constructor () {
     super();
 
-    this.players = new ArraySchema();
+    this.playerJSONs = new ArraySchema();
   }
 }
 schema.defineTypes(RoomState, {
   round: 'number',
   gamePhase: 'string',
-  players: [ PlayerState ],
+  playerJSONs: [ 'string' ],
   theWord: 'string', 
 });
 
